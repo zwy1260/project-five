6bcc1fa053c6f6d65f7360613f2158301821af4a
zwy1260 <100684732+zwy1260@users.noreply.github.com>
2022-05-25 20:41:59 +0800

    返回数据统一格式

---
+++ Result.java
@@ -0,0 +1,44 @@
+package com.abc.rkodemo.utils;
+
+import java.io.Serializable;
+
+/**
+ * 封装返回结果
+ */
+public class Result implements Serializable{
+    private boolean flag;//执行结果，true为执行成功 false为执行失败
+    private String msg;//返回结果信息
+    private Object data;//返回数据
+    public Result(boolean flag, String msg) {
+        super();
+        this.flag = flag;
+        this.msg = msg;
+    }
+
+    public Result(boolean flag, String msg, Object data) {
+        this.flag = flag;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+    public String getMsg() {
+        return msg;
+    }
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
